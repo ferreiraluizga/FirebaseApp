a89a1581a56fe76a8163b9534c0f388d11300b94
ferreiraluizga <luiz.gabriel.lcf@gmail.com>
2025-05-23 12:27:31 -0300

    feat: project files structure

--- app/src/main/java/com/example/firebaseapp/MainActivity.kt
+++ app/src/main/java/com/example/firebaseapp/MainActivity.kt
@@ -10,6 +10,7 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.node.ModifierNodeElement
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.firebaseapp.ui.theme.FirebaseAppTheme
 
@@ -20,28 +21,9 @@ class MainActivity : ComponentActivity() {
         setContent {
             FirebaseAppTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    FirebaseAppTheme {
-        Greeting("Android")
     }
 }
