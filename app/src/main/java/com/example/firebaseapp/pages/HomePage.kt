f045737976503fdae852ca70a6abcfda7b8f422a
ferreiraluizga <luiz.gabriel.lcf@gmail.com>
2025-05-29 09:06:04 -0300

    fix: home page start

--- app/src/main/java/com/example/firebaseapp/pages/HomePage.kt
+++ app/src/main/java/com/example/firebaseapp/pages/HomePage.kt
@@ -6,26 +6,14 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
-import com.example.firebaseapp.AuthState
 import com.example.firebaseapp.AuthViewModel
 
 @Composable
 fun HomePage(modifier: Modifier, navController: NavController, authViewModel: AuthViewModel) {
-
-    val authState = authViewModel.authState.observeAsState()
-
-    LaunchedEffect(authState.value) {
-        when(authState.value) {
-            is AuthState.Authenticated -> navController.navigate("login")
-            else -> Unit
-        }
-    }
 
     Column(
         modifier = modifier.fillMaxSize(),
